8c35b0bd018f21dad1f621280545e1adcdac3878
jesus macias benitez <jesus.macias@meltsan.com>
2017-08-10 14:20:18 -0500

    4to commit

--- src/main/java/com/mx/springboot/controller/PendingsController.java
+++ src/main/java/com/mx/springboot/controller/PendingsController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +30,6 @@ public class PendingsController {
 	}
 
 	@GetMapping("/lista-pendientes")
-	@CrossOrigin(origins = "http://localhost:4200")
 	public List<Pending> allPendings() {
 
 		return listaPendientes();
